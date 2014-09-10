a34fdb26185eb0dcf278cdbc8a6b71830b9f8ec1
Allison O'Connor <aoconnor@gmu.edu>
2014-09-09 23:14:17 -0400

    attempts at a chess board not yet finished

---
+++ chessboard.js
@@ -0,0 +1,10 @@
+var hash = "#"
+var space = " "
+var output =""
+var size = "8"
+for (var line = 1; line <= size; line += 1){
+if (line % 2 ==0)
+  output = hash + space 
+  
+  
+  console.log (output);
