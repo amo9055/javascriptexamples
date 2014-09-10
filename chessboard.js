89cf1a853bfe081d692eeb15f94863db4b81934d
Allison O'Connor <aoconnor@gmu.edu>
2014-09-10 08:06:56 -0400

    single line of hash marks and spaces is working here

--- chessboard.js
+++ chessboard.js
@@ -1,10 +1,10 @@
-var hash = "#"
-var space = " "
-var output =""
 var size = "8"
-for (var line = 1; line <= size; line += 1){
-if (line % 2 ==0)
-  output = hash + space 
-  
-  
-  console.log (output);
+var text = "";
+    var i = 1;
+    while (i <= size) 
+    
+    {
+        text += "# ";
+        i++;
+    }
+  console.log (text);
