465d54b1ffe527b63c2cb177e5dc38cee71fe983
Allison O'Connor <aoconnor@gmu.edu>
2014-09-15 11:53:40 -0400

    display array works, then added SUM function

--- range.js
+++ range.js
@@ -1,3 +1,28 @@
+/* trying to simply accomplish the first task so I have a small piece to build on.*/
+
+var list = []
+function range(start, end) {
+	for (var i = start; i <= end; i ++)
+	list.push(i)
+	
+	return list;
+
+}
+
+function sum(list) {
+  var total = 0;
+  for (var i = 0; i < list.length; i++)
+    total += list[i];
+  return total;
+}
+
+console.log(sum(range(1, 10)));
+
+
+
+
+/* this wasn't working...
+
 function range(start, end) {
 	var array = [];
 	for (var i = start; i >= end; i ++)
@@ -12,4 +37,6 @@ function sum(array) {
   return total;
 }
 
-console.log(sum(range(1, 10)));
+document.write(sum(range(1, 10)));
+
+*/
