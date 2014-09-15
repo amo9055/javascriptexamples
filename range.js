9bdc417efdbcf6cbb3374949a9da05bd7f5ef044
Allison O'Connor <aoconnor@gmu.edu>
2014-09-14 21:53:51 -0400

    first attempt at exercise, which doesn't work

---
+++ range.js
@@ -0,0 +1,15 @@
+function range(start, end) {
+	var array = [];
+	for (var i = start; i >= end; i ++)
+      array.push(i);
+      return array;
+}
+
+function sum(array) {
+  var total = 0;
+  for (var i = 0; i < array.length; i++)
+    total += array[i];
+  return total;
+}
+
+console.log(sum(range(1, 10)));
