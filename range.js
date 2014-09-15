0aa66a4c1162c3764a47ecc3431e4a3a4b331436
Allison O'Connor <aoconnor@gmu.edu>
2014-09-15 12:02:25 -0400

    comments about possible incorrect variable location

--- range.js
+++ range.js
@@ -1,5 +1,9 @@
 /* trying to simply accomplish the first task so I have a small piece to build on.*/
 
+
+/* I think this var list = [] should go inside the function, 
+but when I put it there, it doesn't work. 
+Is this location going to cause a problem?*/
 var list = []
 function range(start, end) {
 	for (var i = start; i <= end; i ++)
@@ -8,7 +12,7 @@ function range(start, end) {
 	return list;
 
 }
-
+/*second piece */
 function sum(list) {
   var total = 0;
   for (var i = 0; i < list.length; i++)
@@ -18,7 +22,7 @@ function sum(list) {
 
 console.log(sum(range(1, 10)));
 
-
+console.log(range(5, 2, -1));
 
 
 /* this wasn't working...
